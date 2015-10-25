c0e881dbc1d64a41f2fb6bdd4a67d4bcd67d119c
Gyuwon Yi <gyuwon@live.com>
2015-10-26 02:23:52 +0900

    ContactList 기본 코드를 추가합니다.

---
+++ ContactList/ContactEditorViewModel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Flip;
+
+namespace ContactList
+{
+    public class ContactEditorViewModel : ObservableObject
+    {
+        private Contact _model;
+        private string _editName;
+        private string _editEmail;
+
+        public ContactEditorViewModel(Contact model)
+        {
+            _model = model;
+            _editName = model.Name;
+            _editEmail = model.Email;
+        }
+
+        public Contact Model
+        {
+            get { return _model; }
+            set { SetValue(ref _model, value); }
+        }
+
+        public string EditName
+        {
+            get { return _editName; }
+            set { SetValue(ref _editName, value); }
+        }
+
+        public string EditEmail
+        {
+            get { return _editEmail; }
+            set { SetValue(ref _editEmail, value); }
+        }
+
+        public IReactiveCommand SaveCommand => null;
+    }
+}
---
+++ ContactList/ContactViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Flip;
+using Flip.ViewModels;
+
+namespace ContactList
+{
+    public class ContactViewModel : ObservableObject
+    {
+        private Contact _model;
+
+        public ContactViewModel(Contact model)
+        {
+            _model = model;
+        }
+
+        public Contact Model
+        {
+            get { return _model; }
+            set { SetValue(ref _model, value); }
+        }
+    }
+}

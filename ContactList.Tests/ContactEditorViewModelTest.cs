32402d2a28af77bf48d3f2be1a9e590c1c086b70
Gyuwon Yi <gyuwon@live.com>
2015-10-26 03:16:17 +0900

    ContactEditorViewModel.SaveCommand 명령에 대한 테스트를 작성합니다.

---
+++ ContactList.Tests/ContactEditorViewModelTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Flip;
+using FluentAssertions;
+using Ploeh.AutoFixture.Xunit2;
+using Xunit;
+
+namespace ContactList.Tests
+{
+    public class ContactEditorViewModelTest
+    {
+        [Theory, AutoData]
+        public void SaveCommandEmitsNewModelRevision(ContactEditorViewModel sut)
+        {
+            Contact next = null;
+            Stream<Contact, int>
+                .Connect(sut.Model.Id)
+                .Subscribe(x => next = x);
+
+            sut.SaveCommand.Execute(null);
+
+            next.Should().NotBeNull();
+            next.Name.Should().Be(sut.EditName);
+            next.Email.Should().Be(sut.EditEmail);
+        }
+    }
+}
